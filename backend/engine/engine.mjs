b19502cb2de052613460e1261fb3e3dcd70cd9d6
Tommaso <jackiedifant@gmail.com>
2021-02-05 19:37:50 +0100

    .

--- backend/engine/engine.mjs
+++ backend/engine/engine.mjs
@@ -32,7 +32,9 @@ export async function registerIo(io_) {
 
         var serialized = BABYLON.SceneSerializer.Serialize(scene);
         const _serialized = 'data:' + JSON.stringify(serialized);
+        console.log('streaming scene');
         socket.emit('stream-scene', _serialized);
+        console.log('scene streamed');
 
         // stream from client
         socket.on('client-stream-mesh', async (meshes) => {
