b26f1d42431199fbef0dae714d396583a266969e
Tommaso <jackiedifant@gmail.com>
2021-02-28 13:10:01 +0100

    changes

--- backend/startup/engine.mjs
+++ backend/startup/engine.mjs
@@ -1,8 +1,8 @@
 import * as engine from '../engine/engine.mjs'
-import socket_io from 'socket.io'
+import { Server } from 'socket.io'
 
 export default function(server) {
-    const io = socket_io(server, {
+    const io = new Server(server, {
         transports: ['polling', 'websocket']
     });
     engine.registerIo(io);
